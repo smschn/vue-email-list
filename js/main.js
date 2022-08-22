fd187f10b0b4a2bd84f78f6536c19c213054dda3
Luca Somaschini <somas99@gmail.com>
2022-08-22 16:43:23 +0200

    Setting up environment

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('test');
+var app = new Vue ({
+
+    el: '#app',
+
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( response => {
+            console.log(response);
+        })
+    }
+})
