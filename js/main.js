51ae9780752d881e98f34e9db6d1a995bfb45aa2
Luca Somaschini <somas99@gmail.com>
2022-08-22 16:54:26 +0200

    Generate one mail

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ var app = new Vue ({
 
     el: '#app',
 
+    data: {
+        arrayEmail: []
+    },
+
     mounted() {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then( response => {
             console.log(response);
+            this.arrayEmail.push(response.data.response);
+            console.log(this.arrayEmail);
         })
     }
+
 })
